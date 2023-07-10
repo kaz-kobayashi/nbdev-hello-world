78d464726c2829622fb9365b46b912a1df4a2138
kaz-kobayashi <50687254+kaz-kobayashi@users.noreply.github.com>
2023-07-10 14:45:42 +0900

    Deleted shapefile

--- nbdev_hello_world/_modidx.py
+++ nbdev_hello_world/_modidx.py
@@ -6,6 +6,5 @@ d = { 'settings': { 'branch': 'main',
                 'git_url': 'https://github.com/kaz-kobayashi/nbdev-hello-world',
                 'lib_path': 'nbdev_hello_world'},
   'syms': { 'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')},
-            'nbdev_hello_world.mytest': { 'nbdev_hello_world.mytest.foo': ('gis-density.html#foo', 'nbdev_hello_world/mytest.py'),
-                                          'nbdev_hello_world.mytest.say_hello': ( 'gis-density.html#say_hello',
-                                                                                  'nbdev_hello_world/mytest.py')}}}
+            'nbdev_hello_world.mytest': { 'nbdev_hello_world.mytest.foo': ('core.html#foo', 'nbdev_hello_world/mytest.py'),
+                                          'nbdev_hello_world.mytest.say_hello': ('core.html#say_hello', 'nbdev_hello_world/mytest.py')}}}
